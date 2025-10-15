2a3016cb9eda7a3130d6c7c85e6ee5b06c28af86
Chinzillla <96362668+Chinzillla@users.noreply.github.com>
2025-10-15 13:42:10 -0400

    prob: algomonster range sum query immutable

---
+++ src/range_sum_query_immutable/solution.py
@@ -0,0 +1,15 @@
+class solution:
+    @staticmethod
+    def initialize_prefix_arr(nums: list[int]) -> list[int]:
+        prefix_sum = [0] * (len(nums) + 1)
+
+        current_sum = 0
+        for i in range(1, len(nums) + 1):
+            current_sum += nums[i - 1]
+            prefix_sum[i] = current_sum
+
+        return prefix_sum
+    
+    def range_sum_query_immutable(nums: list[int], left: int, right: int) -> int:
+        prefix_sum = solution.initialize_prefix_arr(nums)
+        return prefix_sum[right + 1] - prefix_sum[left]
